500b56176da91b5fc3d23cc391cc867de3e40fb7
Onur Öztürk <onurozturk93@gmail.com>
2020-07-09 21:12:12 +0300

    Corrected

--- formation_dynamicAcquisition.m
+++ formation_dynamicAcquisition.m
@@ -12,32 +12,31 @@ kv = 10;
 T = 0.01;
 
 q0 = zeros(5,2);
-qout = zeros(1000,5,2);
+qout = zeros(1500,5,2);
 for i = 1:5
     q0(i,:) = [V(i,:)+delta*(rand(1,2)-0.5*ones(1,2))];
 end
 q = q0;
 
-% plot(V(:,1),V(:,2),'o')
-% hold on
-% plot(q0(:,1),q0(:,2),'*')
-
-for t = 1:1000
+for t = 1:1500
     pt = 1+0.5*sin(0.4*t*T);
-    pt_dot = 0.2*cos(0.4*t*T);
     dij = pt*[d12 d13 d13 d12 d12 d12 d12];
-    dij_dot = pt_dot*[d12 d13 d13 d12 d12 d12 d12];
-    dv = dij.*dij_dot;
-    
+
+    if t == 1
+        dij_ = dij;
+    end
+    ddij = (dij-dij_)/T;
+    dij_ = dij;
+    dv = dij.*ddij;
     vd = [1 cos(t*T)];
 
-    Rq = [(q(1,:)-q(2,:))' (q(2,:)-q(1,:))' zeros(2,3);
-          (q(1,:)-q(3,:))' zeros(2,1) (q(3,:)-q(1,:))' zeros(2,2);
-          (q(1,:)-q(4,:))' zeros(2,2) (q(4,:)-q(1,:))' zeros(2,1);
-          (q(1,:)-q(5,:))' zeros(2,3) (q(5,:)-q(1,:))';
-          zeros(2,1) (q(2,:)-q(3,:))' (q(3,:)-q(2,:))' zeros(2,2);
-          zeros(2,2) (q(3,:)-q(4,:))' (q(4,:)-q(3,:))' zeros(2,1);
-          zeros(2,3) (q(4,:)-q(5,:))' (q(5,:)-q(4,:))'];
+    Rq = [(q(1,:)-q(2,:)) (q(2,:)-q(1,:)) zeros(1,6);
+          (q(1,:)-q(3,:)) zeros(1,2) (q(3,:)-q(1,:)) zeros(1,4);
+          (q(1,:)-q(4,:)) zeros(1,4) (q(4,:)-q(1,:)) zeros(1,2);
+          (q(1,:)-q(5,:)) zeros(1,6) (q(5,:)-q(1,:));
+          zeros(1,2) (q(2,:)-q(3,:)) (q(3,:)-q(2,:)) zeros(1,4);
+          zeros(1,4) (q(3,:)-q(4,:)) (q(4,:)-q(3,:)) zeros(1,2);
+          zeros(1,6) (q(4,:)-q(5,:)) (q(5,:)-q(4,:))];
     
     Rq_plus = Rq'/(Rq*Rq');
 
@@ -52,9 +51,11 @@ for t = 1:1000
     Rqt = Rq_plus;
     for i = 1:5
         k = (-kv*z+dv);
-        Rqi = Rqt(i,1:2)*k(1) + Rqt(i,3:4)*k(2) + Rqt(i,5:6)*k(3) + Rqt(i,7:8)*k(4) ...
-            + Rqt(i,9:10)*k(5) + Rqt(i,11:12)*k(6) + Rqt(i,13:14)*k(7);
-        u(i,:) = Rqi + vd;
+        sum = 0;
+        for j = 1:7
+            sum = sum + Rqt(2*(i-1)+1:2*(i-1)+2,j)*k(j);
+        end
+        u(i,:) = sum' + vd;
     end
 
     q = q+u*T;
@@ -65,9 +66,16 @@ figure(1)
 plot(qout(1,:,1),qout(1,:,2),'.');
 hold on
 grid on
-for t = 2:1000
-    plot(qout(t,:,1),qout(t,:,2),'.');
-end
+% for t = 2:1000
+%     plot(qout(t,:,1),qout(t,:,2),'.');
+% end
+plot(qout(200,:,1),qout(200,:,2),'o');
+plot(qout(400,:,1),qout(400,:,2),'o');
+plot(qout(600,:,1),qout(600,:,2),'o');
+plot(qout(800,:,1),qout(800,:,2),'o');
+plot(qout(1000,:,1),qout(800,:,2),'o');
+plot(qout(1200,:,1),qout(800,:,2),'o');
+plot(qout(1400,:,1),qout(800,:,2),'o');
 plot(qout(end,:,1),qout(end,:,2),'o');
 plot(q0(:,1),q0(:,2),'*');
 hold off
